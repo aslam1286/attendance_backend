c13cd8e304b5fff6cdb5b14707b3ee6cb1d38337
aslam1286 <aslamkhan8620@gmail.com>
2022-06-02 19:01:52 +0530

    changes

--- attendance_app/models.py
+++ attendance_app/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Attendance(models.Model):
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
-    today_date = models.DateTimeField(default=datetime.now)
+    today_date = models.DateField(default=datetime.now)
     from_time = models.TextField(max_length=255)
     to_time = models.TextField(max_length=255)
     total_time = models.TextField(max_length=255)
